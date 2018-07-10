58c6b4f04b6b5a1925044114989e3dafafb7f6ea
Sinevia <info@sinevia.com>
2018-07-10 08:14:18 +0100

    Update Workflow.php

--- src/Workflow.php
+++ src/Workflow.php
@@ -1,4 +1,17 @@
 <?php
+// ========================================================================= //
+// SINEVIA PUBLIC                                        http://sinevia.com  //
+// ------------------------------------------------------------------------- //
+// COPYRIGHT (c) 2008-2018 Sinevia Ltd                   All rights reserved //
+// ------------------------------------------------------------------------- //
+// LICENCE: All information contained herein is, and remains, property of    //
+// Sinevia Ltd at all times.  Any intellectual and technical concepts        //
+// are proprietary to Sinevia Ltd and may be covered by existing patents,    //
+// patents in process, and are protected by trade secret or copyright law.   //
+// Dissemination or reproduction of this information is strictly forbidden   //
+// unless prior written permission is obtained from Sinevia Ltd per domain.  //
+//===========================================================================//
+
 namespace Sinevia\Workflow;
 
 class Workflow {
