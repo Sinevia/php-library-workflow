dffb3585ae110cdf64ff9dfa5e38762fc072e9c6
Sinevia <info@sinevia.com>
2019-02-08 05:57:10 +0000

    Update Workflow.php

--- src/Workflow.php
+++ src/Workflow.php
@@ -1,124 +1,180 @@
 <?php
 
-namespace App\Models\Orders;
+// ========================================================================= //
+// SINEVIA PUBLIC                                        http://sinevia.com  //
+// ------------------------------------------------------------------------- //
+// COPYRIGHT (c) 2008-2019 Sinevia Ltd                   All rights reserved //
+// ------------------------------------------------------------------------- //
+// LICENCE: All information contained herein is, and remains, property of    //
+// Sinevia Ltd at all times.  Any intellectual and technical concepts        //
+// are proprietary to Sinevia Ltd and may be covered by existing patents,    //
+// patents in process, and are protected by trade secret or copyright law.   //
+// Dissemination or reproduction of this information is strictly forbidden   //
+// unless prior written permission is obtained from Sinevia Ltd per domain.  //
+//===========================================================================//
 
-class Order extends \App\Models\BaseModel {
+namespace Sinevia\Workflow;
 
-    protected $table = 'snv_orders_order';
-    protected $primaryKey = 'Id';
+class Workflow {
 
-    const TYPE_DELIVERY = 'Delivery';
-    const TYPE_NONDELIVERY = 'NonDelivery'; // All Suntribe Swimwear orders are NonDelivery
-    const STATUS_COMPLETED = 'Completed';
-    const STATUS_PENDING = 'Pending';
+    private $state = [
+        'current_step' => null,
+        'history' => [],
+        'step_details' => [],
+    ];
+    public $steps = [];
 
-    /**
-     * @return \App\Models\Orders\OrderWorkflow
-     */
-    public function workflow() {
-        $workflow = new OrderWorkflow($this);
-
-        /* 1. No step? Set first step to send vendor purchase orders */
-        if ($workflow->getCurrentStep() == null) {
-            $workflow->setCurrentStep(OrderWorkflow::STEP_SEND_VENDOR_PURCHASE_ORDERS);
-            $workflow->save();
-        }
-
-        /* 2. Purchase orders sent? Move to step to add tracking URLs */
-        if ($workflow->getCurrentStep()->name == OrderWorkflow::STEP_SEND_VENDOR_PURCHASE_ORDERS) {
-            $purchaseOrders = PurchaseOrder::byOrderId($this->Id);
-            if (count($purchaseOrders) > 0) {
-                $workflow->setCurrentStep(OrderWorkflow::STEP_ADD_TRACKING_URLS);
-                $workflow->save();
-            }
-        }
-
-        /* 3. All tracking URLs added? Move to step order fulfilled */
-        if ($workflow->getCurrentStep()->name == OrderWorkflow::STEP_ADD_TRACKING_URLS) {
-            $allItems = \App\Models\Orders\LineItem::where('OrderId', $this->Id)->count();
-            $completedItems = \App\Models\Orders\LineItem::where('OrderId', $this->Id)->whereNotNull('TrackingNumber')->count();
-            if ($allItems == $completedItems) {
-                $workflow->setCurrentStep(OrderWorkflow::STEP_MARK_ORDER_AS_FULFILLED);
-                $workflow->save();
-            }
-        }
-
-        /* 4. Order copleted? */
-        if ($workflow->getCurrentStep()->name == OrderWorkflow::STEP_MARK_ORDER_AS_FULFILLED) {
-            if ($this->Status == self::STATUS_COMPLETED) {
-                $result = $workflow->markStepAsCompleted(OrderWorkflow::STEP_MARK_ORDER_AS_FULFILLED);
-                $workflow->save();
-            }
-        }
-
-        return $workflow;
+    function __construct() {
+        
     }
 
     /**
-     * @param string $shopifyId
-     * @return Order
+     * Adds a step to the workflow
      */
-    public static function findByShopifyId($shopifyId) {
-        return self::where('ShopifyOrderId', $shopifyId)->first();
+    public function addStep($step) {
+        $this->steps[$step->name] = $step;
+        if ($this->state['current_step'] == null) {
+            $this->setCurrentStep($step->name);
+        }
     }
 
     /**
-     * @return int|null
+     * Returns the current step of the workflow
+     * @return Step|null the current step or null otherwise
      */
-    public static function getLastShopifyOrderId() {
-        $query = "CAST(ShopifyOrderId AS UNSIGNED) DESC";
-        $lastOrder = \App\Models\Orders\Order::orderByRaw($query)->first();
+    function getCurrentStep() {
+        $stepName = $this->state['current_step'];
+        if ($stepName == null) {
+            return null;
+        }
+        return $this->steps[$stepName];
+    }
 
-        if ($lastOrder != null) {
-            return $lastOrder->ShopifyOrderId;
+    /**
+     * Sets the current step of the workflow
+     * @param Step|string the Step instance or the name (key) of the Step
+     * @return void
+     */
+    function setCurrentStep($nextStep) {
+        /* Old step log as completed */
+        $currentStep = $this->getCurrentStep();
+        if ($currentStep != null) {
+            $this->state['step_details'][$currentStep->name]['completed'] = date('Y-m-d H:i:s');
         }
 
+        /* New step log as started */
+        $nextStepName = is_string($nextStep) ? $nextStep : $nextStep->name;
+        if (isset($this->steps[$nextStepName]) == false) {
+            throw new \RuntimeException('Step ' . $nextStepName . ' DOES NOT exist in workflow');
+        }
+        $this->state['current_step'] = $nextStepName;
+        $this->state['history'][] = $nextStepName;
+        $this->state['step_details'][$nextStepName]['started'] = date('Y-m-d');
+    }
+
+    /**
+     * Returns true if the specified step is current, false otherwise
+     * @param Step|string the Step instance or the name (key) of the Step
+     * @return void
+     */
+    function isStepCurrent($step) {
+        $stepName = is_string($step) ? $step : $step->name;
+        if ($this->state['current_step'] == $stepName) {
+            return true;
+        }
+        return false;
+    }
+
+    function isStepComplete($step) {
+        $stepName = is_string($step) ? $step : $step->name;
+        $currentStepPosition = array_search($this->state['current_step'], array_keys($this->steps));
+        $stepPosition = array_search($stepName, array_keys($this->steps));
+        // Are we at next step? Yes => Then this step is complete.
+        if ($stepPosition < $currentStepPosition) {
+            return true;
+        }
+        // Is this step marked as compete? Yes => Then this step is complete.
+        if (isset($this->state['step_details'][$stepName]['completed'])) {
+            return true;
+        }
+        return false;
+    }
+
+    function getProgress() {
+        $stepName = $this->state['current_step'];
+        $currentStepPosition = array_search($stepName, array_keys($this->steps));
+        $pending = count($this->steps) - ($currentStepPosition);
+        if ($this->isStepComplete($stepName)) {
+            $pending++;
+        }
+        $percents = ($currentStepPosition / count($this->steps)) * 100;
+        if ($this->isStepComplete($stepName)) {
+            $percents = (($currentStepPosition + 1) / count($this->steps)) * 100;
+        }
+        return [
+            'total' => count($this->steps),
+            'completed' => $currentStepPosition,
+            'current' => $currentStepPosition,
+            'pending' => count($this->steps) - ($currentStepPosition),
+            'percents' => $percents
+        ];
+    }
+
+    public function getSteps() {
+        return $this->steps;
+    }
+
+    public function getStep($name) {
+        if (isset($this->steps[$name])) {
+            return $this->steps[$name];
+        }
         return null;
     }
 
-    /**
-     * Returns the deserialized Shopify order
-     * @return object
-     */
-    public function shopifyOrder() {
-        return json_decode($this->ShopifyOrder);
+    public function getStepMeta($step, $key) {
+        $stepName = is_string($step) ? $step : $step->name;
+        if (isset($this->state['step_details'][$stepName]) == false) {
+            return null;
+        }
+        if (isset($this->state['step_details'][$stepName][$key]) == false) {
+            return null;
+        }
+        return $this->state['step_details'][$stepName][$key];
     }
 
     /**
-     * @return boolean
+     * Sets the current step of the workflow
+     * @param Step|string the Step instance or the name (key) of the Step
+     * @return void
      */
-    public static function tableCreate() {
-        $o = new self;
-
-        if (\Schema::connection($o->connection)->hasTable($o->table) == false) {
-            return \Schema::connection($o->connection)->create($o->table, function (\Illuminate\Database\Schema\Blueprint $table) use ($o) {
-                        $table->engine = 'InnoDB';
-                        $table->string($o->primaryKey, 40)->primary();
-                        $table->string('ShopifyOrderId', 50);
-                        $table->string('Status', 20)->default('Pending');
-                        $table->string('Type', 50)->default('Normal');
-                        $table->datetime('FormEmailSentAt')->nullable();
-                        $table->string('FormEmailSentTo', 255)->default('');
-                        $table->text('FormResponse')->nullable();
-                        $table->datetime('FormResponseAt')->nullable();
-                        $table->text('ShopifyOrder')->nullable();
-                        $table->text('State')->nullable();
-                        $table->datetime('CreatedAt')->nullable();
-                        $table->datetime('UpdatedAt')->nullable();
-                        $table->datetime('DeletedAt')->nullable();
-                        $table->index(['ShopifyOrderId', 'Status', 'Type']);
-                    });
+    function markStepAsCompleted($step) {
+        $stepName = is_string($step) ? $step : $step->name;
+        if (isset($this->state['step_details'][$stepName]) == false) {
+            return false;
         }
 
+        $this->state['step_details'][$stepName]['completed'] = date('Y-m-d H:i:s');
         return true;
     }
 
-    /**
-     * @return boolean
-     */
-    public static function tableDelete() {
-        $o = new self;
-        return \Schema::connection($o->connection)->drop($o->table);
+    public function setStepMeta($step, $key, $value) {
+        $stepName = is_string($step) ? $step : $step->name;
+        if (isset($this->state['step_details'][$stepName]) == false) {
+            $this->state['step_details'][$stepName] = [];
+        }
+        $this->state['step_details'][$stepName][$key] = $value;
+    }
+
+    function getState() {
+        return $this->memory['state'];
+    }
+
+    function fromString($string) {
+        $this->state = json_decode($string, true);
+    }
+
+    function toString() {
+        return json_encode($this->state);
     }
 
 }
