7923a81aacbe5f6b3729ece2337135297943dc29
Sinevia <info@sinevia.com>
2022-03-26 00:33:46 +0000

    Added phpunit

---
+++ tests/WorkflowTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class WorkflowTest extends \PHPUnit\Framework\TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+
+    public function testTrue(){
+        $this->assertTrue(true);
+    }
+}
